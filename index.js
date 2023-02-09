94ce8a6fc2c8029f796c5878266550a8d49deba0
ShaunMoran <57466726+ShaunMoran@users.noreply.github.com>
2023-02-09 14:11:23 -0600

    updates to code. fixed bugs before starting on interface

--- index.js
+++ index.js
@@ -1,66 +1,60 @@
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
-// const path = require('path');
-const url =  require('url');
-
-//////////////////////////////////////////files
-
-// //Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt' , 'utf-8');
-// console.log(textIn)
-// const textOut = 'This is what we know vout the avocado: ${textIn}.\nCreated on ${Date.now()}';
-// fs.writeFileSync('./txt/output.txt' , textOut);
-// console.log('file written!');
-
-//non-blocking, asynchronous way
-// fs.readFile('./txt/start.txt' , 'utf-8', (err, data1) => {
-//     fs.readFile(`./txt/${data1}.txt` , 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile(`./txt/append.txt` , 'utf-8', (err, data3) => {
-//             console.log(data3);
-            
-//             fs.writeFile('./txt/final.txt' , `${data2}\n${data3}` , 'utf-8' , err => {
-//                 console.log('Your file has been written ');
-//             })
-//         });
-//     });
-// });
-// console.log('Will read file!');
-// server
+const url = require('url');
 
 
 
 
-const server = http.createServer((req, res) => {
-const pathName = req.url;
-
-if(pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW');
-}else if (pathName === '/product'){
-    res.end('this is the PRODUCT');
-    } else if (pathName === '/api'){
-
-    fs.readFile('./dev-data/data.json', 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { 'Content-type': 'application/json'})
-      res.end('data');
-    });
-
-      
-    }else {
-      
-      res.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'hello-world'
-      }); 
-      res.end('<h1> Page not found!</h1>');
-    }
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json` , 'utf-8') 
+const dataObj = JSON.parse(data);
+
+const server = http.createServer ((req, res) => {
+    const pathName = req.url;
+
+    if(pathName ===  '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {'Content-type': 'application-json'})
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>')
+        }
 });
 
-server.listen(8000, '127.0.0.1' , () => {
+
+server.listen(8000, '127.0.0.1' , () => {;
     console.log('listening to requests on port 8000');
 });
+
